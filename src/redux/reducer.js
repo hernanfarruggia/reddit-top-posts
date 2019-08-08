29051ed4dd3fa13a283c68f4b480535fff203a41
Hernan Faarruggia <hernan.farruggia@gmail.com>
2019-08-08 12:31:34 -0300

    - Added initial Redux structure

---
+++ src/redux/reducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+    posts: [],
+    selectedPost: null,
+};
+
+const reducer = (state = initialState, action) => {
+
+    switch (action.type) {
+
+        default:
+            return state;
+    }
+};
+
+export default reducer;
